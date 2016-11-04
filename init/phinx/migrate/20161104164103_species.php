d70168b62b62fc625ad90f31d63ad8c239b496d9
Bertold von Dormilich <dormilich@netscape.net>
2016-11-04 18:07:44 +0100

    added tables for boni granted by species selection.

---
+++ init/phinx/migrate/20161104164103_species.php
@@ -0,0 +1,67 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class Species extends AbstractMigration
+{
+    public function change()
+    {
+        $this->table( 'SpeciesMod' )
+            ->addColumn( 'str', 'integer', [
+                'default' => 0,
+            ] )
+            ->addColumn( 'dex', 'integer', [
+                'default' => 0,
+            ] )
+            ->addColumn( 'con', 'integer', [
+                'default' => 0,
+            ] )
+            ->addColumn( 'int', 'integer', [
+                'default' => 0,
+            ] )
+            ->addColumn( 'wis', 'integer', [
+                'default' => 0,
+            ] )
+            ->addColumn( 'cha', 'integer', [
+                'default' => 0,
+            ] )
+            ->addColumn( 'reflex', 'integer', [
+                'default' => 0,
+            ] )
+            ->addColumn( 'fortitude', 'integer', [
+                'default' => 0,
+            ] )
+            ->addColumn( 'will', 'integer', [
+                'default' => 0,
+            ] )
+            ->addColumn( 'reroll', 'integer', [
+                'null' => true,
+                'comment' => 'skill that may be rerolled',
+            ] )
+            ->addColumn( 'focus', 'integer', [
+                'null' => true,
+                'comment' => 'conditional bonus feat (skill focus) for skill',
+            ] )
+            ->addColumn( 'feat', 'integer', [
+                'null' => true,
+                'comment' => 'bonus feat for species',
+            ] )
+            ->addForeignKey( 'id', 'Node', 'id' )
+            ->addForeignKey( 'reroll', 'Node', 'id' )
+            ->addForeignKey( 'focus', 'Node', 'id' )
+            ->addForeignKey( 'feat', 'Node', 'id' )
+            ->create()
+        ;
+
+        $this->table( 'SpeciesTrait' )
+            ->addColumn( 'species', 'integer' )
+            ->addColumn( 'trait', 'integer' )
+            ->addIndex( [ 'species', 'trait' ], [
+                'unique' => true,
+            ] )
+            ->addForeignKey( 'species', 'Node', 'id' )
+            ->addForeignKey( 'trait', 'Node', 'id' )
+            ->create()
+        ;
+    }
+}
